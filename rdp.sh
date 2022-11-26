10bf931d1548ba57ef11d3d2bbfec368f0434923
NH hackers <74050570+NH-hackers@users.noreply.github.com>
2022-11-26 17:36:24 +0530

    Create rdp.sh

---
+++ rdp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./ngrok/ngrok.exe config add-authtoken 2I5Fp6lMk19FE9WwydO3ylRkZfy_5zPaBhESECau6bMt6TH5x
+./ngrok/ngrok.exe tcp 3389
+
+#echo sucess
+#./ngrok/ngrok.exe authtoken 1kizhCykLZXaokTtJ82xbCCNbCz_5dvTZxFef6mjKEVLfiP8o
+#./ngrok/ngrok.exe tcp 3389
+
+echo sucess
