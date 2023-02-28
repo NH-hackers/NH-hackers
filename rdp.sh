be0c29445fae3256736702dda5bc709dc4e674bc
NH hackers <74050570+NH-hackers@users.noreply.github.com>
2023-02-28 06:03:26 -0500

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-while :; do ./ngrok/ngrok.exe authtoken 2I5Fp6lMk19FE9WwydO3ylRkZfy_5zPaBhESECau6bMt6TH5x; ./ngrok/ngrok.exe tcp 3389
+while :; do ./ngrok/ngrok.exe authtoken 1vvrHiukjZOSWvvtxUNd993aWRK_6LwRgWRhQdVHCRG7yNcg1; ./ngrok/ngrok.exe tcp 3389
 
 
 echo sucess
-./ngrok/ngrok.exe authtoken 1kizhCykLZXaokTtJ82xbCCNbCz_5dvTZxFef6mjKEVLfiP8o
+./ngrok/ngrok.exe authtoken 1vvrHiukjZOSWvvtxUNd993aWRK_6LwRgWRhQdVHCRG7yNcg1
 ./ngrok/ngrok.exe tcp 3389
 
 echo sucess
